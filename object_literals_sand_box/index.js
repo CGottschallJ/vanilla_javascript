aeec1bd8dc3fae2ca8386dc8e57585e36cbb1956
CGottschallJ <Cameron.Gottschall@gmail.com>
2018-04-18 21:40:34 -0500

    initial object literals

--- object_literals_sand_box/index.js
+++ object_literals_sand_box/index.js
@@ -1,1 +1,7 @@
 console.log("Hello JavaScript");
+
+/* OBJECT LITERALS */
+  // - Objects allow us to store multiple key/value pairs.
+    // - A key/value pair consists of a property name and a value.
+    // - The property name should describe something a property of the object.
+    // - The value is the value of that property
