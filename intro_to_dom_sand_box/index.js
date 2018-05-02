a8b691cf814c052273323097722a939c5fa01d44
CGottschallJ <Cameron.Gottschall@gmail.com>
2018-05-02 09:14:15 -0500

    addition of DOM notes. TODO: Document Object

--- intro_to_dom_sand_box/index.js
+++ intro_to_dom_sand_box/index.js
@@ -1,1 +1,24 @@
-alert("Hello Javascript");
+/* THE DOCUMENT OBJECT MODEL (DOM) */
+
+  // - The Document Object Model is a structured tree/chart of elements created by the browser representing the elements displayed on the page.
+    // - The top of the tree is the entire document.
+    // - This is followed by the html element (root element) as this is the first element on the page.
+    // - The html element of the tree has two child components (siblings) which are the head tag and body tag.
+    // - Subsequently, any tags created within either of those elements in the HTML document will stem from its respective node and so on and so on.
+  // - JavaScript can be used to manipulate each element.
+  // - Each element/node is Object Oriented meaning it has it's own set of properties and methods for each.
+    // - Properties and Methods can therefore be added as well.
+  // - The document object is a property of the window object.
+
+  /* THE DOCUMENT OBJECT */
+  console.log("! - Document Object Model - !");
+
+    // - The document keyword represents the HTML loaded into the web browser.
+    // - There are numerous methods that can be used in marriage with the document keyword.
+      // - https://www.w3schools.com/jsref/dom_obj_document.asp
+    
+    console.log("Document - ", document);
+
+
+  
+
